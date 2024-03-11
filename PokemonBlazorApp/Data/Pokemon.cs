47287c77bd9fc5775f8e0d68a899029c93bebe49
Sam B <98593125+lazyeyesam@users.noreply.github.com>
2024-03-11 13:14:16 +0000

    Reduced duplicate code, simplying the add/edit process into one form

--- PokemonBlazorApp/Data/Pokemon.cs
+++ PokemonBlazorApp/Data/Pokemon.cs
@@ -1,17 +1,44 @@
-﻿namespace PokemonBlazorApp.Data
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PokemonBlazorApp.Data
 {
     public class Pokemon
     {
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
+        [Required]
         public string Type { get; set; }
+
+        [Required]
+        [Range(1, 600, ErrorMessage = "Total must be between 1 and 600")]
         public int Total { get; set; }
+
+        [Required]
+        [Range(1, 200, ErrorMessage = "HP must be between 1 and 200")]
         public int HP { get; set; }
+
+        [Required]
+        [Range(1, 200, ErrorMessage = "Attack must be between 1 and 200")]
         public int Attack { get; set; }
+
+        [Required]
+        [Range(1, 200, ErrorMessage = "Defence must be between 1 and 200")]
         public int Defence { get; set; }
+
+        [Required]
+        [Range(1, 200, ErrorMessage = "Speed must be between 1 and 200")]
         public int Speed { get; set; }
+
+        [Required]
+        [Range(1, 200, ErrorMessage = "Generation must be between 1 and 200")]
         public int Generation { get; set; }
+
+        [Required]
         public bool Legendary { get; set; }
+
         public List<Trainer> Trainers { get; set; }
     }
 }
