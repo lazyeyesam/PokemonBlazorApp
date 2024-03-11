d268cc0d23b5f225aa4fac2b82b0780f7ae16f53
Sam B <98593125+lazyeyesam@users.noreply.github.com>
2024-03-11 16:11:41 +0000

    Added trainers

--- PokemonBlazorApp/Data/Trainer.cs
+++ PokemonBlazorApp/Data/Trainer.cs
@@ -1,9 +1,13 @@
-﻿namespace PokemonBlazorApp.Data
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PokemonBlazorApp.Data
 {
     public class Trainer
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public Pokemon Companion { get; set; }
     }
 }
