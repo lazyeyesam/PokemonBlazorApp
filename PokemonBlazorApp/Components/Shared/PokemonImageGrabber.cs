bb6ab85d245feb86ae1560f80d363b7197b5430b
Sam B <98593125+lazyeyesam@users.noreply.github.com>
2024-03-14 16:23:27 +0000

    Changed to parameter binding instead of refresh. Added string adjustments to ImageGrabber

--- PokemonBlazorApp/Components/Shared/PokemonImageGrabber.cs
+++ PokemonBlazorApp/Components/Shared/PokemonImageGrabber.cs
@@ -8,7 +8,31 @@ namespace PokemonBlazorApp.Components.Shared
         {
             if (pokemon is null) return null;
 
-            var name = pokemon.Name.Split("Mega")[0].Replace("'", string.Empty).ToLowerInvariant();
+            var name = pokemon.Name.Split("Mega")[0]
+                .Replace("'", string.Empty)
+                .Replace("♀", "-f")
+                .Replace("♂", "-m")
+                .Replace(". ", "-")
+                .Replace("Plant Cloak", "")
+                .Replace("Primal Kyogre", "")
+                .Replace("Primal Groudon", "")
+                .Replace("Normal Forme", "")
+                .Replace("Attack Forme", "")
+                .Replace("Defence Forme", "")
+                .Replace("Speed Forme", "")
+                .Replace("Hoopa Confined", "")
+                .Replace("Hoopa Unbound", "")
+                .Replace("Average Size", "")
+                .Replace("Small Size", "")
+                .Replace("Large Size", "")
+                .Replace("Super Size", "")
+                .Replace("Blade Forme", "")
+                .Replace("Shield Forme", "")
+                .Replace("MeowsticMale", "Meowstic-Male")
+                .Replace("MeowsticFemale", "Meowstic-Female")
+                .Replace(" Forme", "")
+                .ToLowerInvariant();
+
             var imageUrl = $"https://img.pokemondb.net/artwork/large/{name}.jpg";
             return imageUrl;
         }
