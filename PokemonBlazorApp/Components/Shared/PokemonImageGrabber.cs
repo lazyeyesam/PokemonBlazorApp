bbab05bdd427da7f5b16055a554d917423109fb4
Sam B <98593125+lazyeyesam@users.noreply.github.com>
2024-03-22 09:04:28 +0000

    Fixed to show mega pokemon

--- PokemonBlazorApp/Components/Shared/PokemonImageGrabber.cs
+++ PokemonBlazorApp/Components/Shared/PokemonImageGrabber.cs
@@ -10,36 +10,42 @@ namespace PokemonBlazorApp.Components.Shared
         {
             if (pokemon is null) return NOT_FOUND;
 
-            var name = pokemon.Name.Split("Mega")[0]
-                .Replace("'", string.Empty)
-                .Replace("♀", "-f")
-                .Replace("♂", "-m")
-                .Replace(". ", "-")
-                .Replace("Plant Cloak", "")
-                .Replace("Primal Kyogre", "")
-                .Replace("Primal Groudon", "")
-                .Replace("Normal Forme", "")
-                .Replace("Attack Forme", "")
-                .Replace("Defence Forme", "")
-                .Replace("Speed Forme", "")
-                .Replace("Hoopa Confined", "")
-                .Replace("Hoopa Unbound", "")
-                .Replace("Average Size", "")
-                .Replace("Small Size", "")
-                .Replace("Large Size", "")
-                .Replace("Super Size", "")
-                .Replace("Blade Forme", "")
-                .Replace("Shield Forme", "")
-                .Replace("MeowsticMale", "Meowstic-Male")
-                .Replace("MeowsticFemale", "Meowstic-Female")
-                .Replace(" Forme", "")
-                .Replace("Heat Rotom", "")
-                .Replace("Wash Rotom", "")
-                .Replace("Frost Rotom", "")
-                .Replace("Fan Rotom", "")
-                .Replace("Mow Rotom", "")
-                .Replace(" Jr.", "-jr")
-                .ToLowerInvariant();
+            var parts = pokemon.Name.Split("Mega");
+            var name = parts[0];
+            if (parts.Length > 1)
+                name = $"{name}-mega";
+            else
+            {
+                name = name.Replace("'", string.Empty)
+                        .Replace("♀", "-f")
+                        .Replace("♂", "-m")
+                        .Replace(". ", "-")
+                        .Replace("Plant Cloak", "")
+                        .Replace("Primal Kyogre", "")
+                        .Replace("Primal Groudon", "")
+                        .Replace("Normal Forme", "")
+                        .Replace("Attack Forme", "")
+                        .Replace("Defence Forme", "")
+                        .Replace("Speed Forme", "")
+                        .Replace("Hoopa Confined", "")
+                        .Replace("Hoopa Unbound", "")
+                        .Replace("Average Size", "")
+                        .Replace("Small Size", "")
+                        .Replace("Large Size", "")
+                        .Replace("Super Size", "")
+                        .Replace("Blade Forme", "")
+                        .Replace("Shield Forme", "")
+                        .Replace("MeowsticMale", "Meowstic-Male")
+                        .Replace("MeowsticFemale", "Meowstic-Female")
+                        .Replace(" Forme", "")
+                        .Replace("Heat Rotom", "")
+                        .Replace("Wash Rotom", "")
+                        .Replace("Frost Rotom", "")
+                        .Replace("Fan Rotom", "")
+                        .Replace("Mow Rotom", "")
+                        .Replace(" Jr.", "-jr");
+            }
+            name = name.ToLowerInvariant();
 
             var imageUrl = $"https://img.pokemondb.net/artwork/large/{name}.jpg";
             return imageUrl;
